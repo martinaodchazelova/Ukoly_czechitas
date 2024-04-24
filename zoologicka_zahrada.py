e9d6624b8e22eda6f8dced504c61ad4ede508906
Martina Odchazelova <martina.odchazelova@.gmail.com>
2024-04-24 21:43:48 +0200

    Cast Zvire hotova

--- zoologicka_zahrada.py
+++ zoologicka_zahrada.py
@@ -19,3 +19,36 @@ pavouk_export = pavouk.export_to_dict()
 assert pavouk_export['jmeno'] == 'Adolf'
 assert pavouk_export['druh'] == 'Tarantule Velká'
 assert pavouk_export['vaha'] == 0.1
+
+# print(pavouk)
+# print(pavouk_export)
+
+zvirata_dict = [
+    {'jmeno': 'Růženka', 'druh': 'Panda Velká', 'vaha': 150},
+    {'jmeno': 'Vilda', 'druh': 'Vydra Mořská', 'vaha': 20},
+    {'jmeno': 'Matýsek', 'druh': 'Tygr Sumaterský', 'vaha': 300},
+    {'jmeno': 'Karlík', 'druh': 'Lední medvěd', 'vaha': 700},
+]
+
+zvirata = []
+for z in zvirata_dict:
+    zvire = Zvire(z['jmeno'], z['druh'], z['vaha'])
+    zvirata.append(zvire)
+
+# kontrolni funkce pro vytisknutí listu zvirata:
+def vytisknout_zvirata(seznam_zvirat):
+    for jedno_zvire in seznam_zvirat:
+        print(jedno_zvire)
+vytisknout_zvirata(zvirata)
+
+# Zvire class
+zvire = Zvire('Láďa', 'Koala', 15)
+assert hasattr(zvire, 'jmeno')
+assert hasattr(zvire, 'druh')
+assert hasattr(zvire, 'vaha')
+assert isinstance(zvire.jmeno, str)
+assert isinstance(zvire.druh, str)
+assert isinstance(zvire.vaha, int)
+assert zvire.export_to_dict() == {'jmeno': 'Láďa', 'druh': 'Koala', 'vaha': 15}
+
+
