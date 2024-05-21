9a7fd5a699642b17065ad00780626388e40bc5e0
Martina Odchazelova <martina.odchazelova@.gmail.com>
2024-05-21 11:01:52 +0200

    1_verze

--- ukol_2.py
+++ ukol_2.py
@@ -1,9 +1,14 @@
 import requests
 import json
 
-kocky_fakta_api = requests.get('https://cat-fact.herokuapp.com/facts')
-info_o_kockach = kocky_fakta_api.json() 
-print(info_o_kockach)
+try:
+#kocky_fakta_api = requests.get('https://cat-fact.herokuapp.com/facts')
+# endpoint: requests.get('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=10')
+    kocky_fakta_api = requests.get('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=10', timeout = 0.3)
+    info_o_kockach = kocky_fakta_api.json() 
+    print(info_o_kockach)
+except requests.exceptions.Timeout: #HTTPSConnectionPool(host='cat-fact.herokuapp.com', port=443): 
+    print(f'Jste přílič nedočkaví.')
 
 seznam_faktu_o_kockach = []
 
@@ -25,11 +30,10 @@ for text_faktu in seznam_faktu_o_kockach:
     ocislovany_fakt = f"{cislovani}. {text_faktu}"
     ocislovany_seznam_faktu_o_kockach.append(ocislovany_fakt)
     cislovani += 1
+    if cislovani > 10:
+        break 
 
 print(ocislovany_seznam_faktu_o_kockach)
 
-# Výpis očíslovaného seznamu faktů
-# for fakt in ocislovany_seznam_faktu_o_kockach:
-#     print(fakt)
-
-
+with open ('kocici_fakta.json', mode= 'w', encoding='utf-8') as output_file:
+    json.dump(ocislovany_seznam_faktu_o_kockach, output_file, indent=4,ensure_ascii=False)
